fb073068800a19326d753295d0ac23c9c4b3eba4
Bojana Bojic <bojanabojic30@gmail.com>
2023-10-16 03:20:53 +0200

    Create header component

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Logo from '../logo'
 import shoppingCard from '../../assets/images/shopping-cart.svg'
 
 import styles from './Navbar.module.scss'
@@ -8,11 +7,12 @@ import styles from './Navbar.module.scss'
 function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<Logo />
 			<Link to="onama">O nama</Link>
 			<Link to="pasta">Pasta</Link>
 			<Link to="contact">Contact</Link>
-			<img className={styles.icon} src={shoppingCard} alt="shoppingCard" />
+			<Link to="shop">
+				<img className={styles.icon} src={shoppingCard} alt="shoppingCard" />
+			</Link>
 		</nav>
 	)
 }
