9179fb856b9dd7a6ec3199c4ff0f1703f6fba28c
Bojana Bojic <bojanabojic30@gmail.com>
2023-10-20 20:00:06 +0200

    Clean up

--- src/view/home/index.jsx
+++ src/view/home/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Header from '../../components/header'
-import Carousel from '../../components/carousel/index'
 
 function Home() {
 	return (
 		<div>
 			<Header />
-			<Carousel />
 		</div>
 	)
 }
