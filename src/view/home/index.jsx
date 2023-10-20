c23815d9ef4c5310d21b7878230e00050f35e24f
Bojana Bojic <bojanabojic30@gmail.com>
2023-10-20 19:59:20 +0200

    Create UI carousel component

--- src/view/home/index.jsx
+++ src/view/home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/header'
+import Carousel from '../../components/carousel/index'
 
 function Home() {
 	return (
 		<div>
 			<Header />
+			<Carousel />
 		</div>
 	)
 }
